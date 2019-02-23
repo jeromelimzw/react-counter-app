aeef7bcb73bc7e5aeba19c5e756ba202a35d214e
jeromelimzw <jerome.lim.zw@gmail.com>
2019-02-23 22:21:25 +0800

    fixed typo in CounterList

--- src/CounterList.js
+++ src/CounterList.js
@@ -1,24 +1,29 @@
 import React from "react";
 import Counter from "./Counter";
 
-const CounterList = ({ counters, handleDecrement,handleIncrement,handleDelete }) => {
+const CounterList = ({
+  counters,
+  handleDecrement,
+  handleIncrement,
+  handleDelete
+}) => {
   return (
     <div className="bg-lightest-blue pa1 br3">
       {counters.map(item => {
         return (
           <Counter
-            key={itme.id}
+            key={item.id}
             id={item.id}
             value={item.value}
             handleDecrement={() => {
               handleDecrement(item.id);
             }}
             handleIncrement={() => {
-                handleIncrement(item.id);
-              }}
-              handleDelete={() => {
-                handleDelete(item.id);
-              }}
+              handleIncrement(item.id);
+            }}
+            handleDelete={() => {
+              handleDelete(item.id);
+            }}
           />
         );
       })}
